362897056e638125eb57fc4c85f2005c5b247b18
Selesito <selesitomorozov@gmail.com>
2021-01-17 15:09:01 +0400

    1. Паспорт и жители [#436309]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+        Citizen rsl = null;
+        for (String key : citizens.keySet()) {
+            if (passport.contains(key)) {
+                rsl = citizens.get(key);
+                break;
+            }
+        }
+        return rsl;
+    }
+}
