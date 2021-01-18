b94e13e6b5e8ce6a31f1d2ce4737f9bc6ec2ec27
Selesito <selesitomorozov@gmail.com>
2021-01-18 22:40:39 +0400

    1. Организовать сортировку User [#436296]

---
+++ src/main/java/ru/job4j/collection/UserSort.java
@@ -0,0 +1,10 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class UserSort implements Comparator<User> {
+    @Override
+    public int compare(User first, User second) {
+        return first.compareTo(second);
+    }
+}
