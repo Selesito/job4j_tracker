f7b4e900f9af232f76324ce3840afe95dd378517
Selesito <selesitomorozov@gmail.com>
2021-01-18 20:48:14 +0400

    3. Банковские переводы [#436310]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,5 +1,6 @@
 package ru.job4j.bank;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,24 +9,49 @@ public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-
+        users.putIfAbsent(user, new ArrayList<Account>());
     }
 
     public void addAccount(String passport, Account account) {
-
+        User user = findByPassport(passport);
+        List<Account> acc = users.get(user);
+        if (!acc.contains(account.getRequisite())) {
+            acc.add(account);
+        }
     }
 
     public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (passport.contains(user.getPassport())) {
+                return user;
+            }
+        }
         return null;
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        return null;
+        Account result = null;
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> acc = users.get(user);
+            for (Account rsl : acc) {
+                if (rsl.getRequisite().contains(requisite)) {
+                    result = rsl;
+                }
+            }
+        }
+        return result;
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
+        Account src = findByRequisite(srcPassport, srcRequisite);
+        Account dest = findByRequisite(destPassport, destRequisite);
+        if (!src.equals(null) && src.getBalance() >= amount) {
+            dest.setBalance(dest.getBalance() + amount);
+            src.setBalance(src.getBalance() - amount);
+        }
         return rsl;
     }
 }
