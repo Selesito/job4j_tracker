c9c76d9736630bce44de3043beb62a88b52e003c
Selesito <selesitomorozov@gmail.com>
2021-01-22 11:24:41 +0400

    Применение @Before

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -74,6 +74,7 @@ public final class Tracker {
         boolean result = index != -1;
         if (result) {
             items.remove(index);
+            ids--;
         }
         return result;
     }
