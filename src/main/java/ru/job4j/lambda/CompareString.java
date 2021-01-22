a3768e328508cd6c47be6379a97eeda4c33528bd
Selesito <selesitomorozov@gmail.com>
2021-01-22 17:10:39 +0400

    2.2. Лямбда блок [#436125]

--- src/main/java/ru/job4j/lambda/CompareString.java
+++ src/main/java/ru/job4j/lambda/CompareString.java
@@ -1,16 +1,12 @@
 package ru.job4j.lambda;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
-public class FI {
+public class CompareString {
     public static void main(String[] args) {
         Comparator<String> comparator = (left, right) -> {
             System.out.println("compareTo - " + left + " : " + right);
             return right.compareTo(left);
         };
     }
-
 }
