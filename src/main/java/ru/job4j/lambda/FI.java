0f29d9d4bb9c678926b76af393f1edf98ddfebc7
Selesito <selesitomorozov@gmail.com>
2021-01-22 17:05:59 +0400

    2.2. Лямбда блок [#436125]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class FI {
+    public static void main(String[] args) {
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("compareTo - " + left + " : " + right);
+            return right.compareTo(left);
+        };
+        List<String> strs = Arrays.asList("A", "B", "C", "D", "F");
+        Collections.sort(strs, comparator);
+        for (String str : strs) {
+            System.out.println(str);
+        }
+    }
+
+}
