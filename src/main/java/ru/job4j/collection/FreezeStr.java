15538c024e2da83876596de0259f2b4f8bb072f1
Selesito <selesitomorozov@gmail.com>
2021-02-06 10:33:48 +0400

    2. Проверить две строчки на идентичность.

---
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -0,0 +1,34 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FreezeStr {
+    public static boolean eq(String left, String right) {
+        boolean result = true;
+        HashMap<String, Integer> collectLeft = comp(new ArrayList<>(Arrays.asList(left.split(""))));
+        HashMap<String, Integer> collRight = comp(new ArrayList<>(Arrays.asList(right.split(""))));
+        if (!collectLeft.equals(collRight)) {
+            result = false;
+        }
+        return result;
+    }
+
+    public static HashMap<String, Integer> comp(ArrayList<String> collect) {
+        HashMap<String, Integer> result = new HashMap<>();
+        for (String value : collect) {
+            int count = 0;
+            if (!result.containsKey(value)) {
+                for (String value1 : collect) {
+                    if (value.equals(value1)) {
+                        count++;
+                    }
+                }
+            }
+            result.put(value, count);
+        }
+        return result;
+    }
+}
